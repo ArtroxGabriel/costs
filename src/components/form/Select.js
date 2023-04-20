dc8cb0c1d8b29fcd0a0ce02a072a50673d08583d
ArtroxGabriel <artroxgabriel@gmail.com>
2023-04-20 11:18:40 -0300

    oI

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -4,7 +4,7 @@ function Select( {name, text, options, handleOnchange, value} ) {
     return (
             <div className={styles.form_control}>
                 <label htmlFor={name}>{text}:</label>
-                <select id={name} name={name} onChange={handleOnchange} value={value}>
+                <select id={name} name={name} onChange={handleOnchange} value={value || ''}>
                     <option>Selecione uma opção:</option>
                     {options.map( (option) => (
                         <option 
